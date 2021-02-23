02fd15b15f8beb955cf6259f4c563362c618b3dd
p1aton <ovk.job@gmail.com>
2021-02-23 18:40:42 +0300

    add file

---
+++ convert.py
@@ -0,0 +1,15 @@
+import hashlib
+import base58
+import binascii
+
+with open("/home/nex/resultGit1.txt", "r") as f:    #Input file path
+      for line in f:
+        # private_key_WIF = input("WIF: ")
+        first_encode = base58.b58decode(line)
+        private_key_full = binascii.hexlify(first_encode)
+        private_key = private_key_full[2:-8]
+
+        print(private_key)
+
+# grep -E -o '5[HJK][1-9A-HJ-NP-Za-km-z]{49}' dir/test.txt > goal.txt
+# grep -E -o '[KL][1-9A-HJ-NP-Za-km-z]{51}' dir/test > goal.txt
